04963e515cd254ae76439690201ee657afe0e8c0
mayakats <nalubegaramulah52@gmail.com>
2023-11-04 03:17:03 -0700

    changes in django work(models.py and admin.py)

--- wbsites/models.py
+++ wbsites/models.py
@@ -3,9 +3,14 @@ from django.db import models
 class EquipmentCategory(models.Model):
     category_id = models.AutoField(primary_key=True)
     category_name = models.CharField(unique=True,max_length=45)
+    category_description = models.TextField(blank=True)
     date_created = models.DateField(auto_now=True)
+class Meta:
+    verbose_name_plural="Equipment Categories"
+def __str__(self):
+    return self.category_name
     
-class Products(models.Model):
+class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     category_id = models.ForeignKey(EquipmentCategory,on_delete=models.CASCADE)
     product_name = models.CharField(unique=True,max_length=20)
@@ -14,13 +19,19 @@ class Products(models.Model):
     available_stock = models.IntegerField(default=0)
     unit_measure = models.CharField(unique=True,max_length=10)
     date_updated = models.DateField(auto_now=True)
+class Meta:
+    verbose_name_plural="Products"
+def __str__(self):
+    return self.product_name   
 
-class Transactions(models.Model):
+class Transaction(models.Model):
     transaction_id = models.AutoField(primary_key=True)
-    product_id = models.ForeignKey(Products,on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product,on_delete=models.CASCADE)
     transaction_type = models.CharField(unique=True,max_length=10)
     stock_taken = models.IntegerField(default=0)
     transaction_amount = models.IntegerField(default=0)
     transaction_date = models.DateField(auto_now=True)
+class Meta:
+    verbose_name_plural="Transactions"
     
 
